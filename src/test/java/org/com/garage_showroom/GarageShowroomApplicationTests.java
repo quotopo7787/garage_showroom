f35ec8466ee85ddd0bbe68a0edd1e2c9afa40188
ADMIN-PC\Administrator <quotopo@gmail.com>
2025-09-27 08:58:20 +0700

    Initial commit - Spring Boot Thymeleaf project

---
+++ src/test/java/org/com/garage_showroom/GarageShowroomApplicationTests.java
@@ -0,0 +1,13 @@
+package org.com.garage_showroom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GarageShowroomApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
